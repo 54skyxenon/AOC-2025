732ba6b8ac8c407d1a183c2df3b5128650ec6e4a
Brandon Liang <54skyxenon@gmail.com>
2025-12-09 00:05:20 -0500

    rename days properly

--- day07.pl
+++ day07.pl
@@ -1,4 +1,4 @@
-# https://adventofcode.com/2025/day/6
+# https://adventofcode.com/2025/day/7
 
 ## BEGIN BOILERPLATE
 use strict;
--- day08.pl
+++ day08.pl
@@ -1,4 +1,4 @@
-# https://adventofcode.com/2025/day/6
+# https://adventofcode.com/2025/day/8
 
 ## BEGIN BOILERPLATE
 use strict;
